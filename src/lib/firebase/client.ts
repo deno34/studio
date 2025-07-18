1d73d694f49b08cbe1be898be86a51526fd04873
trapslash media <denismwangi655@gmail.com>
2025-07-18 13:36:53 +0000

    when i try to signup am getting error Google sign-in faied,Firebase:Erro

--- src/lib/firebase/client.ts
+++ src/lib/firebase/client.ts
@@ -9,15 +9,8 @@ import {
   GoogleAuthProvider,
   signOut,
 } from 'firebase/auth';
+import { firebaseConfig } from '@/lib/firebaseConfig';
 
-const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-};
 
 // Initialize Firebase
 const app: FirebaseApp = !getApps().length ? initializeApp(firebaseConfig) : getApp();
